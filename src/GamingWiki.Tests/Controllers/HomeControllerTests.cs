6d4a9dd194fde2545a747bdae4a6f24eb323f4f3
Ati Vassileva <ativassileva@gmail.com>
2021-08-15 18:57:28 +0300

    Added home tests

---
+++ src/GamingWiki.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using GamingWiki.Web.Controllers;
+using GamingWiki.Web.Models;
+using GamingWiki.Web.Models.Home;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+namespace GamingWiki.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexShouldReturnGuestPageViewForUnauthenticatedUser()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View("GuestPage");
+
+        [Fact]
+        public void IndexShouldReturnIndexViewForAuthenticatedUser()
+        => MyController<HomeController>
+            .Instance(controller => controller
+                .WithUser(TestUser.Identifier))
+            .Calling(c => c.Index())
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<HomeViewModel>());
+
+        [Fact]
+        public void AboutShouldReturnView()
+        => MyController<HomeController>
+            .Instance()
+            .Calling(c => c.About())
+            .ShouldReturn()
+            .View();
+
+        [Fact]
+        public void ErrorShouldReturnIndexView()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithUser(TestUser.Identifier))
+                .Calling(c => c.Error())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<ErrorViewModel>());
+    }
+}
