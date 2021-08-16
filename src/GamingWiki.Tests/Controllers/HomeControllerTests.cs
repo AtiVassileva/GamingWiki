339110ec0f038b8ab2bec2c383ce107420c7116f
Ati Vassileva <ativassileva@gmail.com>
2021-08-16 22:12:50 +0300

    Added cache tests

--- src/GamingWiki.Tests/Controllers/HomeControllerTests.cs
+++ src/GamingWiki.Tests/Controllers/HomeControllerTests.cs
@@ -1,8 +1,14 @@
-﻿using GamingWiki.Web.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using GamingWiki.Services.Models.Articles;
+using GamingWiki.Services.Models.Games;
+using GamingWiki.Services.Models.Tricks;
+using GamingWiki.Web.Controllers;
 using GamingWiki.Web.Models;
 using GamingWiki.Web.Models.Home;
 using MyTested.AspNetCore.Mvc;
 using Xunit;
+using static GamingWiki.Web.Common.WebConstants.Cache;
 
 namespace GamingWiki.Tests.Controllers
 {
@@ -22,6 +28,24 @@ namespace GamingWiki.Tests.Controllers
             .Instance(controller => controller
                 .WithUser(TestUser.Identifier))
             .Calling(c => c.Index())
+            .ShouldHave()
+            .MemoryCache(cache => cache
+                .ContainingEntry(entry => entry
+                    .WithKey(LatestArticlesCacheKey)
+                    .WithAbsoluteExpirationRelativeToNow(TimeSpan
+                        .FromMinutes(CacheExpiringTime))
+                    .WithValueOfType<List<ArticleServiceHomeModel>>())
+                .ContainingEntry(entry => entry
+                    .WithKey(LatestGamesCacheKey)
+                    .WithAbsoluteExpirationRelativeToNow(TimeSpan
+                        .FromMinutes(CacheExpiringTime))
+                    .WithValueOfType<List<GameServiceListingModel>>())
+                .ContainingEntry(entry => entry
+                    .WithKey(LatestTricksCacheKey)
+                    .WithAbsoluteExpirationRelativeToNow(TimeSpan
+                        .FromMinutes(CacheExpiringTime))
+                    .WithValueOfType<List<TrickServiceHomeModel>>()))
+            .AndAlso()
             .ShouldReturn()
             .View(view => view
                 .WithModelOfType<HomeViewModel>());
