61ef423ed23262fac33ab6277269477c8555e84f
Ati Vassileva <ativassileva@gmail.com>
2021-08-17 22:50:31 +0300

    Added games pipeline tests

---
+++ src/GamingWiki.Tests/Pipeline/GamesPipelineTests.cs
@@ -0,0 +1,275 @@
+﻿using GamingWiki.Services.Models.Games;
+using GamingWiki.Web.Controllers;
+using GamingWiki.Web.Models.Games;
+using static GamingWiki.Tests.Data.Games;
+using static GamingWiki.Web.Areas.Admin.AdminConstants;
+using MyTested.AspNetCore.Mvc;
+using Shouldly;
+using Xunit;
+
+namespace GamingWiki.Tests.Pipeline
+{
+    public class GamesPipelineTests
+    {
+        [Fact]
+        public void AllWithoutPageShouldReturnCorrectViewWithCorrectModel()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/All")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c => c.All(With.No<int>()))
+            .Which(controller => controller
+                .WithData(FiveGames))
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>()
+                .Passing(gameListing =>
+                {
+                    gameListing.Games.Count.ShouldBe(0);
+                    gameListing.Games.PageIndex.ShouldBe(0);
+                    gameListing.Games.TotalPages.ShouldBe(0);
+                }));
+
+        [Theory]
+        [InlineData(1, 0, 0)]
+        public void AllWithPageShouldReturnCorrectViewWithCorrectModel(int pageIndex, int expectedCountOnPage, int totalPages)
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Games/All?pageIndex={pageIndex}")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<GamesController>(c => c.All(pageIndex))
+                .Which(controller => controller
+                    .WithData(FiveGames))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<GameFullModel>()
+                    .Passing(gameListing =>
+                    {
+                        gameListing.Games.Count.ShouldBe(expectedCountOnPage);
+                        gameListing.Games.PageIndex.ShouldBe(pageIndex);
+                        gameListing.Games.TotalPages.ShouldBe(totalPages);
+                    }));
+
+        [Fact]
+        public void GetCreateShouldBeMappedAndReturnCorrectView()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation("/Games/Create")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<GamesController>(c => c.Create())
+                .Which()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<GameFormModel>());
+
+        [Fact]
+        public void DetailsShouldBeMappedAndReturnCorrectViewWithValidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Games/Details?gameId={TestGame.Id}")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c => c.Details(TestGame.Id))
+            .Which(controller => controller
+                .WithData(TestGame))
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameServiceDetailsModel>());
+
+        [Fact]
+        public void DetailsShouldBeMappedAndReturnErrorViewWithInvalidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Games/Details?gameId={TestGame.Id}")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c => c.Details(TestGame.Id))
+            .Which()
+            .ShouldReturn()
+            .View("Error");
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnCorrectViewWithValidId()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Games/Edit?gameId={TestGame.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<GamesController>(c => c.Edit(TestGame.Id))
+                .Which(controller => controller
+                    .WithData(TestGame))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<GameServiceEditModel>());
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnUnauthorizedForUnauthorizedUsers()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Games/Edit?gameId={TestGame.Id}")
+                    .WithUser(TestUser.Identifier)
+                    .WithAntiForgeryToken())
+                .To<GamesController>(c => c.Edit(TestGame.Id))
+                .Which(controller => controller
+                    .WithData(TestGame))
+                .ShouldReturn()
+                .Unauthorized();
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnErrorViewWithInvalidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Games/Edit?gameId={TestGame.Id}")
+                .WithUser(TestUser.Identifier)
+                .WithAntiForgeryToken())
+            .To<GamesController>(c => c.Edit(TestGame.Id))
+            .Which()
+            .ShouldReturn()
+            .View("Error");
+
+        [Fact]
+        public void DeleteShouldBeMappedAndRedirectUponSuccessfulAction()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Games/Delete?gameId={TestGame.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<GamesController>(c => c.Delete(TestGame.Id))
+                .Which(controller => controller
+                    .WithData(TestGame))
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<GamesController>(c => c
+                        .All(With.Any<int>())));
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnUnauthorizedForUnauthorizedUsers()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Games/Delete?gameId={TestGame.Id}")
+                .WithUser(TestUser.Identifier)
+                .WithAntiForgeryToken())
+            .To<GamesController>(c => c.Delete(TestGame.Id))
+            .Which(controller => controller
+                .WithData(TestGame))
+            .ShouldReturn()
+            .Unauthorized();
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnErrorViewWithInvalidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Games/Delete?gameId={TestGame.Id}")
+                .WithUser(user => user.InRole(AdministratorRoleName))
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Delete(TestGame.Id))
+            .Which()
+            .ShouldReturn()
+            .View("Error");
+
+        [Fact]
+        public void SearchShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/Search?parameter=a&pageIndex=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Search("a", 1))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>());
+
+        [Fact]
+        public void SearchShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+            .WithLocation("/Games/Search?parameter=a")
+            .WithUser()
+            .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+        c.Search("a", With.No<int>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+            .WithModelOfType<GameFullModel>());
+
+        [Fact]
+        public void FilterShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/Filter?parameter=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Filter(1, With.No<int>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>());
+
+        [Fact]
+        public void FilterShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/Filter?parameter=1&pageIndex=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Filter(1, 1))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>());
+
+        [Fact]
+        public void MineShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/Mine?pageIndex=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Mine(1))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>());
+
+        [Fact]
+        public void MineShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Games/Mine")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<GamesController>(c =>
+                c.Mine(With.No<int>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<GameFullModel>());
+    }
+}
---
+++ src/GamingWiki.Tests/Pipeline/HomePipelineTests.cs
@@ -0,0 +1,26 @@
+﻿using GamingWiki.Web.Controllers;
+using GamingWiki.Web.Models.Home;
+using MyTested.AspNetCore.Mvc;
+using static GamingWiki.Tests.Data.Games;
+using static GamingWiki.Tests.Data.Articles;
+using static GamingWiki.Tests.Data.Tricks;
+using Xunit;
+
+namespace GamingWiki.Tests.Pipeline
+{
+    public class HomePipelineTests
+    {
+        [Fact]
+        public void IndexShouldReturnCorrectViewWithModel()
+        => MyRouting
+            .Configuration()
+            .ShouldMap("/")
+            .To<HomeController>(c => c.Index())
+            .Which(controller => controller
+                .WithData(ArticleTestData))
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<HomeViewModel>()
+                .Passing(articles => articles.Count.ShouldBe(expected)));
+    }
+}
