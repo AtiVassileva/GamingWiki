46e741c4923b4b0a5f929eccb644c1584cc39f59
Ati Vassileva <ativassileva@gmail.com>
2021-08-18 09:58:12 +0300

    Added messages pipeline tests

--- src/GamingWiki.Tests/Pipeline/MessagesPipelineTests.cs
+++ src/GamingWiki.Tests/Pipeline/MessagesPipelineTests.cs
@@ -1,7 +1,65 @@
-﻿namespace GamingWiki.Tests.Pipeline
+﻿using GamingWiki.Models;
+using GamingWiki.Web.Controllers;
+using static GamingWiki.Tests.Data.Messages;
+using static GamingWiki.Web.Common.WebConstants;
+using static GamingWiki.Web.Areas.Admin.AdminConstants;
+using MyTested.AspNetCore.Mvc;
+using System.Linq;
+using GamingWiki.Web.Areas.Admin;
+using Xunit;
+
+namespace GamingWiki.Tests.Pipeline
 {
     public class MessagesPipelineTests
     {
+        [Fact]
+        public void DeleteShouldBeMappedAndRedirectUponSuccessfulAction()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Messages/Delete?messageId={TestMessage.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<MessagesController>(c => c.Delete(TestMessage.Id))
+                .Which(controller => controller
+                    .WithData(TestMessage))
+                .ShouldHave()
+                    .Data(data => data
+                        .WithSet<Message>(messages => messages
+                            .All(m => m.Id != TestMessage.Id)))
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(GlobalMessageKey))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<DiscussionsController>(c => c
+                        .Chat(With.Any<int>())));
 
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnErrorViewWithInvalidId()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Messages/Delete?messageId={TestMessage.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<MessagesController>(c => c.Delete(TestMessage.Id))
+                .Which()
+                .ShouldReturn()
+                .View("Error");
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnUnauthorizedForUnauthorizedUsers()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Messages/Delete?messageId={TestMessage.Id}")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<MessagesController>(c => c.Delete(TestMessage.Id))
+                .Which(controller => controller
+                    .WithData(TestMessage))
+                .ShouldReturn()
+                .Unauthorized();
     }
 }
