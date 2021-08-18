180bcfbec6eaa4f28eb649d814278c713f153fc2
Ati Vassileva <ativassileva@gmail.com>
2021-08-18 09:39:54 +0300

    Added replies pipeline tests

---
+++ src/GamingWiki.Tests/Pipeline/RepliesPipelineTests.cs
@@ -0,0 +1,135 @@
+﻿using System.Linq;
+using GamingWiki.Models;
+using GamingWiki.Web.Controllers;
+using static GamingWiki.Web.Areas.Admin.AdminConstants;
+using static GamingWiki.Web.Common.WebConstants;
+using static GamingWiki.Tests.Data.Replies;
+using static GamingWiki.Tests.Data.Comments;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+namespace GamingWiki.Tests.Pipeline
+{
+    public class RepliesPipelineTests
+    {
+        [Fact]
+        public void AddShouldBeMappedAndRedirectUponSuccessfulAction()
+        => MyPipeline
+            .Configuration()
+        .ShouldMap(request => request
+            .WithMethod(HttpMethod.Post)
+        .WithLocation($"/Replies/Add?commentId={TestComment.Id}")
+            .WithUser()
+            .WithAntiForgeryToken()
+        .WithFormFields(new
+        {
+            TestValidReplyFormModel.ReplyContent
+        }))
+        .To<RepliesController>(c =>
+        c.Add(TestValidReplyFormModel, TestComment.Id))
+            .Which(controller => controller
+                .WithData(TestComment))
+            .ShouldHave()
+            .Data(data => data
+                .WithSet<Reply>(replies => replies
+                    .Any(c =>
+                        c.Content == TestValidReplyFormModel.ReplyContent
+                        && c.CommentId == TestComment.Id)
+                )).TempData(tempData => tempData
+                .ContainingEntryWithKey(GlobalMessageKey))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect(redirect => redirect
+                .To<ArticlesController>(c =>
+                    c.Details(With.Any<int>())));
+
+        [Fact]
+        public void AddShouldBeMappedAndReturnErrorViewWithInvalidCommentId()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation($"/Replies/Add?commentId={TestComment.Id}")
+                    .WithUser()
+                    .WithAntiForgeryToken().WithFormFields(new
+                    {
+                        TestValidReplyFormModel.ReplyContent
+                    }))
+                .To<RepliesController>(c => c
+                    .Add(TestValidReplyFormModel, TestComment.Id))
+                .Which()
+                .ShouldReturn()
+                .View("Error");
+
+        [Fact]
+        public void AddShouldBeMappedAndRedirectUponUnsuccessfulAction()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation($"/Replies/Add?commentId={TestComment.Id}")
+                    .WithUser()
+                    .WithAntiForgeryToken()
+                    .WithFormFields(new
+                    {
+                        TestInvalidReplyFormModel.ReplyContent
+                    }))
+                .To<RepliesController>(c =>
+                    c.Add(TestInvalidReplyFormModel, TestComment.Id))
+                .Which(controller => controller
+                    .WithData(TestComment))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Reply>(replies => replies
+                        .All(c =>
+                            c.Content != TestReply.Content
+                            && c.CommentId != TestComment.Id)
+                    )).TempData(tempData => tempData
+                    .ContainingEntryWithKey(GlobalMessageKey))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<ArticlesController>(c =>
+                        c.Details(With.Any<int>())));
+
+        [Fact]
+        public void DeleteShouldBeMappedAndRedirectUponSuccessfulAction()
+        => MyPipeline
+            .Configuration()
+        .ShouldMap(request => request
+        .WithLocation($"/Replies/Delete?replyId={TestReply.Id}")
+            .WithUser(user => user.InRole(AdministratorRoleName))
+            .WithAntiForgeryToken())
+        .To<RepliesController>(c => c.Delete(TestReply.Id))
+            .Which(controller => controller
+                .WithData(TestReply)
+                .WithData(TestComment))
+            .ShouldHave()
+            .Data(data => data
+                .WithSet<Reply>(replies => replies
+                    .All(c => c.Id != TestReply.Id)
+                ))
+            .TempData(tempData => tempData
+                .ContainingEntryWithKey(GlobalMessageKey))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect(redirect => redirect
+                .To<ArticlesController>(c =>
+                    c.Details(With.Any<int>())));
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnErrorViewWithInvalidId()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation($"/Replies/Delete?replyId={TestReply.Id}")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<RepliesController>(c => c
+                    .Delete(TestReply.Id))
+                .Which()
+                .ShouldReturn()
+                .View("Error");
+    }
+}
---
+++ src/GamingWiki.Tests/Pipeline/TricksPipelineTests.cs
@@ -0,0 +1,272 @@
+﻿using GamingWiki.Services.Models.Tricks;
+using GamingWiki.Web.Controllers;
+using GamingWiki.Web.Models.Tricks;
+using static GamingWiki.Tests.Data.Tricks;
+using static GamingWiki.Web.Areas.Admin.AdminConstants;
+using MyTested.AspNetCore.Mvc;
+using Shouldly;
+using Xunit;
+
+namespace GamingWiki.Tests.Pipeline
+{
+    public class TricksPipelineTests
+    {
+        [Fact]
+        public void AllWithoutPageShouldReturnCorrectViewWithCorrectModel()
+       => MyPipeline
+           .Configuration()
+           .ShouldMap(request => request
+               .WithLocation("/Tricks/All")
+               .WithUser()
+               .WithAntiForgeryToken())
+           .To<TricksController>(c => c.All(With.No<int>()))
+           .Which(controller => controller
+               .WithData(FiveTricks))
+           .ShouldReturn()
+           .View(view => view
+               .WithModelOfType<TrickFullModel>()
+               .Passing(trickListing =>
+               {
+                   trickListing.Tricks.Count.ShouldBe(0);
+                   trickListing.Tricks.PageIndex.ShouldBe(0);
+                   trickListing.Tricks.TotalPages.ShouldBe(2);
+               }));
+
+        [Theory]
+        [InlineData(1, 0, 2)]
+        public void AllWithPageShouldReturnCorrectViewWithCorrectModel(int pageIndex, int expectedCountOnPage, int totalPages)
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Tricks/All?pageIndex={pageIndex}")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c => c.All(pageIndex))
+                .Which(controller => controller
+                    .WithData(FiveTricks))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<TrickFullModel>()
+                    .Passing(trickListing =>
+                    {
+                        trickListing.Tricks.Count.ShouldBe(expectedCountOnPage);
+                        trickListing.Tricks.PageIndex.ShouldBe(pageIndex);
+                        trickListing.Tricks.TotalPages.ShouldBe(totalPages);
+                    }));
+
+        [Fact]
+        public void GetCreateShouldBeMappedAndReturnCorrectView()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Tricks/Create")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c => c.Create())
+                .Which()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<TrickFormModel>());
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnCorrectViewWithValidId()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Tricks/Edit?trickId={TestTrick.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c => c.Edit(TestTrick.Id))
+                .Which(controller => controller
+                    .WithData(TestTrick))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<TrickServiceEditModel>());
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnUnauthorizedForUnauthorizedUsers()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Tricks/Edit?trickId={TestTrick.Id}")
+                    .WithUser(TestUser.Identifier)
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c => c.Edit(TestTrick.Id))
+                .Which(controller => controller
+                    .WithData(TestTrick))
+                .ShouldReturn()
+                .Unauthorized();
+
+        [Fact]
+        public void GetEditShouldBeMappedAndReturnErrorViewWithInvalidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Tricks/Edit?trickId={TestTrick.Id}")
+                .WithUser(TestUser.Identifier)
+                .WithAntiForgeryToken())
+            .To<TricksController>(c => c.Edit(TestTrick.Id))
+            .Which()
+            .ShouldReturn()
+            .View("Error");
+
+        [Fact]
+        public void DeleteShouldBeMappedAndRedirectUponSuccessfulAction()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation($"/Tricks/Delete?trickId={TestTrick.Id}")
+                    .WithUser(user => user.InRole(AdministratorRoleName))
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c => c.Delete(TestTrick.Id))
+                .Which(controller => controller
+                    .WithData(TestTrick))
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<TricksController>(c => c
+                        .All(With.Any<int>())));
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnUnauthorizedForUnauthorizedUsers()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Tricks/Delete?trickId={TestTrick.Id}")
+                .WithUser(TestUser.Identifier)
+                .WithAntiForgeryToken())
+            .To<TricksController>(c => c.Delete(TestTrick.Id))
+            .Which(controller => controller
+                .WithData(TestTrick))
+            .ShouldReturn()
+            .Unauthorized();
+
+        [Fact]
+        public void DeleteShouldBeMappedAndReturnErrorViewWithInvalidId()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation($"/Tricks/Delete?trickId={TestTrick.Id}")
+                .WithUser(user => user.InRole(AdministratorRoleName))
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Delete(TestTrick.Id))
+            .Which()
+            .ShouldReturn()
+            .View("Error");
+
+        [Fact]
+        public void GetSearchShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Tricks/Search?parameter=a&pageIndex=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Search("a", 1, With.No<string>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void GetSearchShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+            .WithLocation("/Tricks/Search?parameter=a")
+            .WithUser()
+            .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+        c.Search("a", With.No<int>(), With.No<string>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+            .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void GetSearchShouldBeMappedAndReturnCorrectViewWithName()
+            => MyPipeline
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithLocation("/Tricks/Search?parameter=a&pageIndex=1&name=test")
+                    .WithUser()
+                    .WithAntiForgeryToken())
+                .To<TricksController>(c =>
+                    c.Search("a", 1, "test"))
+                .Which()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void PostSearchShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Tricks/Search?searchCriteria=abc")
+                .WithMethod(HttpMethod.Post)
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Search("abc", With.No<int>()))
+            .Which()
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void PostSearchShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Tricks/Search?searchCriteria=abc&pageIndex=1")
+                .WithMethod(HttpMethod.Post)
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Search("abc", 1))
+            .Which()
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void MineShouldBeMappedAndReturnCorrectViewWithPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Tricks/Mine?pageIndex=1")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Mine(1))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<TrickFullModel>());
+
+        [Fact]
+        public void MineShouldBeMappedAndReturnCorrectViewWithoutPageIndex()
+        => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Tricks/Mine")
+                .WithUser()
+                .WithAntiForgeryToken())
+            .To<TricksController>(c =>
+                c.Mine(With.No<int>()))
+            .Which()
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<TrickFullModel>());
+    }
+}
